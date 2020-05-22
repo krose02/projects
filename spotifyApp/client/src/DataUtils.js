59f707460178cfb4afc3c67e50e6f0ebd1ad3ce0
kiara.rose <kiara.rose@mlb.com>
2020-05-22 15:34:25 -0400

    Restructured DataUtils.js; Added more functions  to get user data

--- spotifyApp/client/src/DataUtils.js
+++ spotifyApp/client/src/DataUtils.js
@@ -1,23 +1,19 @@
 /*
- * This files contains functions all
- * functions that will be used in App.js
+ * This files contains functions all
+ * functions that will be used in App.js
  */
-import { useState, useEffect } from "react";
+/************ spotify api ***************/
 import Spotify from "spotify-web-api-js";
 const spotifyWebApi = new Spotify();
-const user_ids = [
-  "coolkid_casey",
-  "carolinegarrido",
-  "127323676",
-  "dx9apszk636qozmp7g25m6uog",
-  "iloveeemusic",
-  "nickifrankel"
-];
 
-var urls = [];
+/************** params *****************/
 
-/* hash function that will return the access_token and refresh_token */
-const getHashParams = () => {
+const params = getHashParams();
+spotifyWebApi.setAccessToken(params.access_token);
+
+/* hash function that will return the 
+   access_token and refresh_token */
+function getHashParams() {
   var hashParams = {};
   var e,
     r = /([^&;=]+)=?([^&;]*)/g,
@@ -26,26 +22,68 @@ const getHashParams = () => {
     hashParams[e[1]] = decodeURIComponent(e[2]);
   }
   return hashParams;
-};
-const params = getHashParams();
-
-/* Checking to see if user is logged in */
-function CheckLogIn() {
-  /* Setting intitial log in state of user to false */
-  const [loggedIn, setLoggedIn] = useState(false);
-  useEffect(() => {
-    if (params.access_token) {
-      /* library stores access token */
-      spotifyWebApi.setAccessToken(params.access_token);
-      setLoggedIn(true);
-    }
-  }, [loggedIn]);
-  return loggedIn;
 }
+/************* user info ****************/
 
-/* populating urls based on user_ids */
-for (var i = 0; i < user_ids.length; i++) {
-  urls.push(`https://api.spotify.com/v1/users/${user_ids[i]}/playlists`);
+const user_ids = [
+  "coolkid_casey",
+  "carolinegarrido",
+  "127323676",
+  "dx9apszk636qozmp7g25m6uog",
+  "iloveeemusic",
+  "nickifrankel"
+];
+const urls = getUrls(user_ids);
+/* getting the names of the users from their profile */
+const user_names = getUserNames(user_ids);
+/* creating urls based on user_ids */
+function getUrls(user_ids) {
+  var urls = [];
+  for (var i = 0; i < user_ids.length; i++) {
+    urls.push(`https://api.spotify.com/v1/users/${user_ids[i]}/playlists`);
+  }
+  return urls;
 }
+/* getting names of the users */
+function getUserNames(user_ids) {
+  var user_names = [];
+  user_ids.forEach(user => {
+    fetch(`https://api.spotify.com/v1/users/${user}`, {
+      method: "GET",
+      headers: { Authorization: "Bearer " + params.access_token }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(user => {
+        user_names.push(user.display_name);
+      });
+  });
+  return user_names;
+}
+/* fetching playlists from users */
+// function GetPlaylists() {
+//   const [data, setData] = useState([]);
+//   useEffect(() => {
+//     async function fetchData(url) {
+//       await fetch(url, {
+//         method: "GET",
+//         headers: { Authorization: "Bearer " + params.access_token }
+//       })
+//         .then(response => {
+//           return response.json();
+//         })
+//         .then(playlists => {
+//           data.push(playlists.items);
+//           setData(data);
+//         });
+//     }
+//     urls.forEach(url => {
+//       fetchData(url);
+//     });
+//   });
+//   console.log(data);
+//   return data;
+// }
 
-export { urls, params, CheckLogIn, GetPlaylists };
+export { getHashParams, spotifyWebApi, user_names };
