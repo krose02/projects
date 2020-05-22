823b4652e4d6d105504cadf7202346cd4db5d6d6
kiara.rose <kiara.rose@mlb.com>
2020-05-22 15:32:28 -0400

    Changed structure of App.js

--- spotifyApp/client/src/App.js
+++ spotifyApp/client/src/App.js
@@ -1,26 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import Header from "./Components/Header";
-import NowPlaying from "./Components/NowPlaying";
-
-import Spotify from "spotify-web-api-js";
-import { urls, params, CheckLogIn, GetPlaylists } from "./DataUtils";
-
-const spotifyWebApi = new Spotify();
+import LogIn from "./Components/LogIn";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const loggedIn = CheckLogIn();
-  const data = GetPlaylists();
-
   return (
     <div className="App">
-      <a href="http://localhost:8888">
-        {!loggedIn && <button> Log into spotify</button>}
-      </a>
-      {loggedIn && (
-        <Header text="Let's See What Your Friends Are Listening To" />
-      )}
-      {loggedIn && <NowPlaying />}
+      <LogIn />
+      {/* <NowPlaying /> */}
     </div>
   );
 }
